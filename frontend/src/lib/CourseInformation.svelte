4968a834da525fe2354f550c4e2af52beeeb3841
Lars Francke <lars.francke@gmail.com>
2023-02-05 23:22:13 +0100

    A bit of clean up

---
+++ frontend/src/lib/CourseInformation.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  let courseCode: string | null = null;
+</script>
+
+<input type="text" placeholder="Course code" class="input w-full max-w-xs" bind:value="{courseCode}"/>
+
+{#if courseCode === null}
+  <p>Please enter a course code to get detailed information</p>
+{:else}
+  <ul>
+    <li><a href="https://murmelbahn.fly.dev/course/{courseCode}/dump">Course dump (JSON)</a></li>
+    <li><a href="https://murmelbahn.fly.dev/course/{courseCode}/bom">Bill of Materials (JSON)</a></li>
+    <li><a href="https://murmelbahn.fly.dev/course/{courseCode}/bom?format=csv" target="_blank">Bill of Materials
+      (Gravisheet CSV)</a></li>
+  </ul>
+{/if}
---
+++ frontend/src/lib/InventoryEditor.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+    import {onMount} from 'svelte';
+    import {Set} from "./models/Set";
+    import {Inventory} from "./models/Inventory";
+
+    let data: Record<string, Set> | null = null;
+    let buildable = null;
+
+    onMount(async () => {
+        const res = await fetch('http://localhost:3000/set/list');
+        data = await res.json();
+    });
+
+    function onSubmit(e) {
+        const formData: FormData = new FormData(e.target);
+
+        const data: Inventory = {
+            extra_elements: undefined,
+            sets: {}
+        };
+        for (let field of formData) {
+            const [key, value] = field;
+            data.sets[key] = Number(value);
+        }
+        doPost(data);
+    }
+
+    async function doPost(data) {
+        const res = await fetch('http://localhost:3000/buildable', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+
+        buildable = await res.json();
+    }
+</script>
+
+{#if data === null}
+    <p>Loading...</p>
+{:else}
+    <form on:submit|preventDefault={onSubmit}>
+        <ul>
+            {#each Object.entries(data) as [key, value]}
+                <li>
+                    <input class="input input-sm input-bordered w-24" type="number" value="0" name="{key}" id="set-{key}"/>
+                    <label for="set-{key}">{value.id}</label>
+                </li>
+            {/each}
+        </ul>
+        <button class="btn" type="submit">Submit</button>
+    </form>
+{/if}
+
+{#if buildable !== null}
+    <h2>Tracks you can build</h2>
+    <ul>
+        {#each Object.entries(buildable) as [key, value]}
+            <li>{value}</li>
+        {/each}
+    </ul>
+{/if}
